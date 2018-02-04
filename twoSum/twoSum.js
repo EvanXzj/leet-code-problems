3ee85ff4432bce5e058629c5e69faf334003b08e
EvanXzj <2686465170@qq.com>
2018-02-04 15:56:37 +0800

    add time logger to twoSum.js

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -1,4 +1,4 @@
-var twoSum = function(nums, target) {
+let twoSum = function(nums, target) {
   if (nums.includes(target)) {
     return [nums.indexOf(target)]
   } else {
@@ -14,7 +14,27 @@ var twoSum = function(nums, target) {
   return 'no two elements adds equal target'
 }
 
-let nums = [3, 2, 4],
-  target = 3
+let twoSum2 = (nums, target) => {
+  let myMap = new Map()
+  let temp
+  for (let i = 1; i < nums.length; i++) {
+    myMap.set(nums[i], i)
+  }
 
+  for (let i = 0; i < nums.length; i++) {
+    temp = target - nums[i]
+
+    if (myMap.has(temp) && myMap.get(temp) !== i) {
+      return [i, myMap.get(temp)]
+    }
+  }
+}
+let nums = [3, 2, 4, 5, 9, 10, 20, 44, 8, 11, 64, 55, 59, 13, 21, 33, 40, 73, 81, 69],
+  target = 6
+console.time('twoSum')
 console.log(twoSum(nums, target))
+console.timeEnd('twoSum')
+
+console.time('twoSum2')
+console.log(twoSum2(nums, target))
+console.timeEnd('twoSum2')
