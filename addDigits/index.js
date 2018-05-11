6d2960b42055e8f532d3f440c4b85ef33be69c6b
EvanXzj <2686465170@qq.com>
2018-05-11 14:22:51 +0800

    add digits done

---
+++ addDigits/index.js
@@ -0,0 +1,14 @@
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+// Example:
+
+// Input: 38
+// Output: 2
+// Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
+//              Since 2 has only one digit, return it.
+
+const addDigits = function(num) {
+  return num <= 9 ? num : num % 9 || 9
+}
+
+// why 9 ?
