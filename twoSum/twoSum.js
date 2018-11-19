63f0a9fc1c1f756f048aac5abc9464080190f691
EvanXzj <2686465170@qq.com>
2018-11-19 10:23:40 +0800

    第二次解两数之和

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -18,18 +18,38 @@ let twoSum2 = (nums, target) => {
   let myMap = new Map()
   let temp
   for (let i = 1; i < nums.length; i++) {
-    myMap.set(nums[i], i)
+    myMap.set(nums[i], i) // 问题1： 如果相同的元素，只会保存最后一个元素的下标值， 如果这题变成求所有的组合应该怎么做？
   }
 
   for (let i = 0; i < nums.length; i++) {
     temp = target - nums[i]
 
     if (myMap.has(temp) && myMap.get(temp) !== i) {
+      // 问题2：  [2, 4, 4, 5],  应该输出[0, 1], 结果输出了[0,2]
       return [i, myMap.get(temp)]
     }
   }
 }
-let nums = [3, 2, 4, 5, 9, 10, 20, 44, 8, 11, 64, 55, 59, 13, 21, 33, 40, 73, 81, 69],
+
+// 在遍历插入元素到哈希表的同时去判断是否存在可用结果， 可以解决上面的问题2
+const twoSum3 = (nums, target) => {
+  const map = new Map()
+
+  let remainder
+  for (let i = 0; i < nums.length; i++) {
+    remainder = target - nums[i]
+
+    if (map.has(remainder)) {
+      return [map.get(remainder), i]
+    }
+
+    map.set(nums[i], i)
+  }
+
+  return 'No elements was found'
+}
+
+let nums = [3, 2, 4, 5, 9, 10, 20, 44, 8, 11, 64, 55, 59, 13, 21, 33, 40, 73, 81, 69, 4],
   target = 6
 console.time('twoSum')
 console.log(twoSum(nums, target))
@@ -38,3 +58,7 @@ console.timeEnd('twoSum')
 console.time('twoSum2')
 console.log(twoSum2(nums, target))
 console.timeEnd('twoSum2')
+
+console.time('twoSum3')
+console.log(twoSum3(nums, target))
+console.timeEnd('twoSum3')
